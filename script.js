d18b1309fa8a051d2c26d6070c3a77c1c7dcce87
Alphasam123 <Alphasam123@gmail.com>
2021-07-17 15:38:48 +0100

    refactor js

--- script.js
+++ script.js
@@ -8,70 +8,78 @@ let currentPlayer = playerA;
 
 let won = false;
 
-const place = (box) => {
-    if (box.innerText === `` || won) {
-        box.innerText = currentPlayer;
-        box.className = `${currentPlayer}`;
+let board = [[], [], []];
 
-        checkGameBoard();
-        currentPlayer = currentPlayer === playerA ? playerB : playerA;
-    }
-};
+const getElementId = (x, y) => `${x},${y}`;
+const getElementCoords = (element) => {
+    const coords = element.id.split(`,`);
 
-const getCoordString = (x, y) => {
-    return `cell(${x}, ${y})`;
-};
-
-const getCell = (x, y) => {
     return {
-        x: x,
-        y: y,
-        element: document.getElementById(getCoordString(x, y)),
+        x: coords[0],
+        y: coords[1],
     };
 };
 
-const getAllCells = () => {
-    const cells = [[], [], []];
+const getElement = (x, y) => document.getElementById(getElementId(x, y));
 
-    for (let x = 0; x <= 2; x++) {
-        for (let y = 0; y <= 2; y++) {
-            cells[x][y] = getCell(x, y);
-        }
+for (let x = 0; x <= 2; x++) {
+    for (let y = 0; y <= 2; y++) {
+        board[x][y] = {
+            element: getElement(x, y),
+            player: undefined,
+        };
     }
+}
 
-    return cells;
+const setPlayer = (cell) => {
+    cell.player = currentPlayer;
+    cell.element.className = `${currentPlayer}`;
+    cell.element.innerText = currentPlayer;
+};
+const setWin = (cell) => {
+    cell.element.className = `winner${cell.element.className}`;
+};
+const setLose = (cell) => {
+    cell.element.className += ` lose`;
+};
+
+const place = (box) => {
+    const coords = getElementCoords(box);
+    const cell = board[coords.x][coords.y];
+
+    if (won) return;
+    if (cell.player) return;
+
+    setPlayer(cell, currentPlayer);
+    checkGameBoard();
+    currentPlayer = currentPlayer === playerA ? playerB : playerA;
 };
 
 const doCellsMatch = (a, b, c) =>
-    a.element.className !== ``
-    && a.element.className === b.element.className
-    && b.element.className === c.element.className;
+    a.player && a.player === b.player && b.player === c.player;
 
 const checkForWin = (a, b, c) => {
     if (doCellsMatch(a, b, c)) {
-        getAllCells().map((row) =>
-            row.map((cell) => (cell.element.className += ` lose`)),
-        );
+        board.forEach((row) => row.forEach(setLose));
 
-        a.element.className = `winner${a.element.className}`;
-        b.element.className = `winner${a.element.className}`;
-        c.element.className = `winner${a.element.className}`;
+        setWin(a);
+        setWin(b);
+        setWin(c);
 
         won = true;
     }
 };
 
 const checkGameBoard = () => {
-    const cells = getAllCells();
-
+    // this can be done with one loop, or two .forEach
     for (let x = 0; x <= 2; x++) {
-        checkForWin(cells[x][0], cells[x][1], cells[x][2]);
+        checkForWin(board[x][0], board[x][1], board[x][2]);
     }
 
     for (let y = 0; y <= 2; y++) {
-        checkForWin(cells[0][y], cells[1][y], cells[2][y]);
+        checkForWin(board[0][y], board[1][y], board[2][y]);
     }
 
-    checkForWin(cells[0][0], cells[1][1], cells[2][2]);
-    checkForWin(cells[0][2], cells[1][1], cells[2][0]);
+    checkForWin(board[0][0], board[1][1], board[2][2]);
+    checkForWin(board[0][2], board[1][1], board[2][0]);
 };
